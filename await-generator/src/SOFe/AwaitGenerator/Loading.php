175ce7abfb170be161db0b7efe2b2e48693668b2
SOFe <sofe2038@gmail.com>
2022-03-04 22:49:49 +0800

    Added the Loading class

---
+++ await-generator/src/SOFe/AwaitGenerator/Loading.php
@@ -0,0 +1,66 @@
+<?php
+
+/*
+ * await-generator
+ *
+ * Copyright (C) 2018 SOFe
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+declare(strict_types=1);
+
+namespace SOFe\AwaitGenerator;
+
+use AssertionError;
+use Closure;
+use Generator;
+use RuntimeException;
+
+/**
+ * `Loading` is a class that represents an asynchronously loaded value.
+ * Users with an instance of `Loading` can call `get` to wait for the loading process to complete.
+ *
+ * @template T
+ */
+final class Loading{
+	/** @var list<Closure(): void>|null */
+	private ?array $onLoaded = [];
+	private $value;
+
+	public function __construct(Closure $loader){
+		Await::f2c(function() use($loader) {
+			$this->value = yield from $loader();
+			$onLoaded = $this->onLoaded;
+			$this->onLoaded = null;
+
+			if($onLoaded === null){
+				throw new AssertionError("loader is called twice on the same object");
+			}
+
+			foreach($onLoaded as $closure){
+				$closure();
+			}
+		});
+	}
+
+	public function get() : Generator{
+		if($this->onLoaded !== null){
+			yield from Await::promise(function($resolve){
+				$this->onLoaded[] = $resolve;
+			});
+		}
+
+		return $this->value;
+	}
+}
